9d7a2bd3b547ad92d37939988a200de832f02a11
lucascencig <cenciglucas@gmail.com>
2023-11-01 16:55:28 -0300

    cors

--- src/Hooks/useSocket.jsx
+++ src/Hooks/useSocket.jsx
@@ -1,27 +1,24 @@
 import { io } from 'socket.io-client';
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
-const socket = io.connect('https://render-chat-back.onrender.com/')
+const socket = io.connect('https://render-chat-back.onrender.com')
 
 export const useSocket = () => {
-
-  const [isConnected, setIsConnected] = useState(false)
-
-  const [messages, setMessages] = useState([])
+  const [isConnected, setIsConnected] = useState(false);
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     socket.on('connect', () => setIsConnected(true));
 
     socket.on('chat_message', (data) => {
-      setMessages(messages => [...messages, data])
-    })
+      setMessages((prevMessages) => [...prevMessages, data]);
+    });
 
     return () => {
-      socket.off('connect')
-      socket.off('chat_message')
-
-    }
-  }, [messages])
+      socket.off('connect');
+      socket.off('chat_message');
+    };
+  }, []);
 
   return { connected: isConnected, messages, socket };
-}
+};
